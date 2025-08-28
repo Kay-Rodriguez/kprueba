0adcde0e6406493d38feb677ba1ae69004da8464
Isaac Quinapallo <isaac.quinapallo@epn.edu.ec>
2025-08-28 10:03:17 -0500

    eliminacion de HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-// src/main.jsx (o index.js, según tu proyecto)
+// src/main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
